a1b872b12700b687e22177826eccd9b1c3f719a8
Andrew Demchuk <demchuk162@gmail.com>
2024-06-14 22:59:11 +0300

    update

--- flopProject/settings.py
+++ flopProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%!w(_9ulk2$41^a4ew$+itx%70y&9v+)1njgb*(z5%+e*(94s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
