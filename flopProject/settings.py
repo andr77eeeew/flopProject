afd77a6c849bd06e97cd53bbccc7a1d044615e74
Andrew Demchuk <demchuk162@gmail.com>
2024-06-14 23:58:34 +0300

    ZAEBAL IDI NAHUI SHMARA LISHAYNAYA14

--- flopProject/settings.py
+++ flopProject/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(%!w(_9ulk2$41^a4ew$+itx%70y&9v+)1njgb*(z5%+e*(94s'
+# SECRET_KEY = 'django-insecure-(%!w(_9ulk2$41^a4ew$+itx%70y&9v+)1njgb*(z5%+e*(94s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['35.160.120.126', '44.233.151.27', '34.211.200.85']
+# ALLOWED_HOSTS = ['35.160.120.126', '44.233.151.27', '34.211.200.85']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -90,9 +93,11 @@ DATABASES = {
         "NAME": BASE_DIR / 'db.sqlite3',
     }
 }
+database_url = os.environ.get('DATABASE_URL')
+# DATABASES['default'] = dj_database_url.parse(
+#     "postgres://flopbase_user:BWiroCFmmV40RZt0ACRNCV7VX5HGJBWB@dpg-cpm9betds78s73bivetg-a.frankfurt-postgres.render.com/flopbase")
 
-DATABASES['default'] = dj_database_url.parse(
-    "postgres://flopbase_user:BWiroCFmmV40RZt0ACRNCV7VX5HGJBWB@dpg-cpm9betds78s73bivetg-a.frankfurt-postgres.render.com/flopbase")
+DATABASES['default'] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
