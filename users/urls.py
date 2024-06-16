2ccce0737fdb978f9f438604888775fc2e984e16
Andrew Demchuk <demchuk162@gmail.com>
2024-06-17 00:22:39 +0300

    updated users url added obtain and refresh token

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from users import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('me/', views.UserDetailView.as_view(), name='user_detail'),
+    path('obtain/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
