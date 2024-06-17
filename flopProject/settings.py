aa905836fa54f084c5e2966ca3e695a8d10d7509
Andrew Demchuk <demchuk162@gmail.com>
2024-06-18 00:12:46 +0300

    updated some settings

--- flopProject/settings.py
+++ flopProject/settings.py
@@ -43,11 +43,11 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=100),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
-    'UPDATE_LAST_LOGIN': False,
+    'UPDATE_LAST_LOGIN': True,
 
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
@@ -166,7 +166,7 @@ DATABASES = {
         "NAME": BASE_DIR / 'db.sqlite3',
     }
 }
-#
+
 database_url = os.environ.get('DATABASE_URL')
 DATABASES['default'] = dj_database_url.parse(database_url)
 
