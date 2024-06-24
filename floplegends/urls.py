7770acedc4c4b81cf011f3881816d65b1ed114d1
Andrew Demchuk <demchuk162@gmail.com>
2024-06-24 23:13:44 +0300

    added delete and update flop legends posts

--- floplegends/urls.py
+++ floplegends/urls.py
@@ -3,5 +3,7 @@ from floplegends import views
 
 urlpatterns = [
     path('create/', views.CreateflopLegendsView.as_view(), name='create'),
+    path('update/<int:id>/', views.UpdateflopLegendsView.as_view(), name='update'),
+    path('delete/<int:id>/', views.DeleteflopLegendsView.as_view(), name='delete'),
     path('all/', views.AllflopLegendsView.as_view(), name='all'),
 ]
--- floplegends/views.py
+++ floplegends/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import NotFound
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from floplegends.models import flopLegendsModel
@@ -18,6 +20,71 @@ class CreateflopLegendsView(generics.CreateAPIView):
         creator = self.request.user
         cover = self.request.data.get('cover')
         serializer.save(creator=creator, cover=cover)
+
+
+class UpdateflopLegendsView(generics.RetrieveUpdateAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = flopLegendsSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self):
+        return flopLegendsModel.objects.filter(creator=self.request.user)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = queryset.filter(id=self.kwargs.get('id')).first()
+        if obj is None:
+            raise NotFound('No object found with the given ID.')
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except NotFound:
+            return Response({"error": "No object found with the given ID."}, status=404)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def patch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.creator == self.request.user:
+            serializer = self.get_serializer(obj, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        return Response({"error": "You do not have permission to perform this action."}, status=403)
+
+
+class DeleteflopLegendsView(generics.DestroyAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = flopLegendsSerializer
+
+    def get_queryset(self):
+        return flopLegendsModel.objects.filter(creator=self.request.user)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = queryset.filter(id=self.kwargs.get('id')).first()
+        if obj is None:
+            raise NotFound('No object found with the given ID.')
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except NotFound:
+            return Response({"error": "No object found with the given ID."}, status=404)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.creator == self.request.user:
+            obj.delete()
+            return Response({"message": "flopLegends deleted successfully."}, status=204)
+        return Response({"error": "You do not have permission to perform this action."}, status=403)
 
 
 class AllflopLegendsView(generics.ListAPIView):
