44b4f5b043f97639050a31f918ece1beab6ccf88
Andrew Demchuk <demchuk162@gmail.com>
2024-06-25 22:45:42 +0300

    updated consumers for chat

--- flopProject/asgi.py
+++ flopProject/asgi.py
@@ -12,7 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 from flopChat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flopProject.settings')
@@ -21,11 +20,10 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
+    "websocket":
         AuthMiddlewareStack(
             URLRouter(
                 websocket_urlpatterns
             )
-        )
     ),
 })
