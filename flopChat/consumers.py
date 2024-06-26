55212d972a5c17bf0ed9b9d48bddc9d99cc98708
Andrew Demchuk <demchuk162@gmail.com>
2024-06-27 00:57:19 +0300

    updated consumer

--- flopChat/consumers.py
+++ flopChat/consumers.py
@@ -8,7 +8,7 @@ from users.models import User
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self, text_data):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
@@ -19,18 +19,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        text_data_json = json.loads(text_data)
-        sender = text_data_json['sender']
-        recipient = text_data_json['recipient']
-
-        messages = await sync_to_async(MessageModel.objects.filter(sender=sender,recipient=recipient)) | await sync_to_async(MessageModel.objects.filter(sender=recipient, recipient=sender))
-        for message in messages:
-            await self.send(text_data=json.dumps({
-                'message': message.content,
-                'sender': message.sender.username,
-                'avatar': message.sender.avatar.url,
-                'recipient': message.recipient.username
-            }))
+        # if 'text_data' in self.scope:
+        #     try:
+        #         text_data_json = json.loads(self.scope['text_data'])
+        #         sender_username = text_data_json['sender']
+        #         recipient_username = text_data_json['recipient']
+        #
+        #         messages = await self.get_messages(sender_username, recipient_username)
+        #
+        #         for message in messages:
+        #             await self.send(text_data=json.dumps({
+        #                 'message': message.content,
+        #                 'sender': message.sender.username,
+        #                 'avatar': message.sender.avatar.url,
+        #                 'recipient': message.recipient.username
+        #             }))
+        #     except json.JSONDecodeError:
+        #         # Handle JSON decoding error
+        #         pass
+        # else:
+        #     # Handle case where 'text_data' is missing
+        #     pass
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -41,12 +50,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        sender_username = text_data_json['sender_username']
-        recipient_username = text_data_json['recipient_username']
+        sender = text_data_json['sender']
+        recipient = text_data_json['recipient']
 
-        if message and sender_username and recipient_username:
-            sender = await sync_to_async(User.objects.get(username=sender_username))
-            recipient = await sync_to_async(User.objects.get(username=recipient_username))
+        if message and sender and recipient:
+            sender = await sync_to_async(User.objects.get(username=sender.username))
+            recipient = await sync_to_async(User.objects.get(username=recipient.username))
 
             msg = MessageModel(sender=sender, recipient=recipient, content=message)
             await sync_to_async(msg.save())
