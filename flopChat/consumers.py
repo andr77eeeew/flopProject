348a1aaa2684253a57e7fa5f287e954dca056105
Andrew Demchuk <demchuk162@gmail.com>
2024-06-27 23:56:14 +0300

    updated consumer5

--- flopChat/consumers.py
+++ flopChat/consumers.py
@@ -19,26 +19,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        if 'get_users' in self.scope:
-            try:
-                text_data_json = json.loads(self.scope['get_users'])
-                sender_username = text_data_json['sender']
-                recipient_username = text_data_json['recipient']
-
-                messages = await self.get_messages(sender_username, recipient_username)
-
-                for message in messages:
-                    await self.send(text_data=json.dumps({
-                        'message': message.content,
-                        'sender': message.sender.username,
-                        'avatar': message.sender.avatar.url,
-                        'recipient': message.recipient.username
-                    }))
-            except json.JSONDecodeError:
-                pass
-        else:
-            pass
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -47,38 +27,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        sender_username = text_data_json['sender']
-        recipient_username = text_data_json['recipient']
+        message_type = text_data_json['type']
 
-        if message and sender_username and recipient_username:
-            sender = await self.get_user(sender_username)
-            recipient = await self.get_user(recipient_username)
+        if message_type == 'get_users':
+            sender_user = text_data_json['sender']
+            recipient_user = text_data_json['recipient']
+            sender = await self.get_user(sender_user)
+            recipient = await self.get_user(recipient_user)
+            messages = await self.get_messages(sender, recipient)
+            for message in messages:
+                await self.send(text_data=json.dumps({
+                    'message': message.content,
+                    'sender': sender.username,
+                    'avatar': sender.avatar.url if sender.avatar.url else None,
+                    'recipient': recipient.username
+                }))
+        elif message_type == 'chat_message':
 
-            if sender.avatar:
+            message = text_data_json['message']
+            sender_username = text_data_json['sender']
+            recipient_username = text_data_json['recipient']
+
+            if message and sender_username and recipient_username:
+                sender = await self.get_user(sender_username)
+                recipient = await self.get_user(recipient_username)
+
                 await self.channel_layer.group_send(
                     self.room_group_name,
                     {
                         'type': 'chat_message',
                         'message': message,
                         'sender': sender.username,
-                        'avatar': sender.avatar.url,
-                        'recipient': recipient.username
-                    }
-                )
-            else:
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'chat_message',
-                        'message': message,
-                        'sender': sender.username,
-                        'avatar': None,
+                        'avatar': sender.avatar.url if sender.avatar.url else None,
                         'recipient': recipient.username
                     }
                 )
 
-            await self.save_message(sender, recipient, message)
+                await self.save_message(sender, recipient, message)
 
     @database_sync_to_async
     def get_user(self, username):
